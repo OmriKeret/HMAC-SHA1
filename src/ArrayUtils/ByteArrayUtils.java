2dc192d3711d74c9947aa6b5751f347a0591b536
OmriKeret <omri.keret@gmail.com>
2015-12-25 14:40:56 +0200

    added byte array utils

--- src/ArrayUtils/ByteArrayUtils.java
+++ src/ArrayUtils/ByteArrayUtils.java
@@ -1,44 +1,53 @@
 package ArrayUtils;
 
 public class ByteArrayUtils {
-	  public static byte[] Concat(this byte[] buffer1, byte[] buffer2)
+	  public static byte[] Concat(byte[] buffer1, byte[] buffer2)
       {
-          return Enumerable.Concat(buffer1, buffer2).ToArray();
+		  byte[] concatedArray = new byte[buffer1.length + buffer2.length];
+		  System.arraycopy(buffer1, 0, concatedArray, 0, buffer1.length);
+          System.arraycopy(buffer2, 0, concatedArray, buffer1.length, buffer2.length);
+          return concatedArray;
+          
       }
 
-      public static byte[] XOR(this byte[] buffer1, byte[] buffer2)
+      public static byte[] XOR(byte[] buffer1, byte[] buffer2)
       {
-          var bigBuffer = buffer1.Length < buffer2.Length ? buffer2 : buffer1;
-          var smallBuffer = buffer1.Length < buffer2.Length ? buffer1 : buffer2;
-          var xoredBuffer = new byte[bigBuffer.Length];
-          for (int i = 0; i < smallBuffer.Length; i++)
+    	  byte[] bigBuffer = buffer1.length < buffer2.length ? buffer2 : buffer1;
+    	  byte[] smallBuffer = buffer1.length < buffer2.length ? buffer1 : buffer2;
+    	  byte[] xoredBuffer = new byte[bigBuffer.length];
+          for (int i = 0; i < smallBuffer.length; i++)
           {
               xoredBuffer[i] = (byte)(bigBuffer[i] ^ smallBuffer[i]);
           }
 
-          int leftBytesStartIndex = smallBuffer.Length;
-          int leftBytesLength = bigBuffer.Length - smallBuffer.Length;
-          Array.Copy(bigBuffer, leftBytesStartIndex, xoredBuffer, smallBuffer.Length, leftBytesLength);
+          int leftBytesStartIndex = smallBuffer.length;
+          int leftBytesLength = bigBuffer.length - smallBuffer.length;
+          System.arraycopy(bigBuffer, leftBytesStartIndex, xoredBuffer, smallBuffer.length, leftBytesLength);
 
           return xoredBuffer;
       }
 
-      public static byte[] Pad(this byte[] buffer, byte paddingByte, int paddingLength)
+      public static byte[] Pad(byte[] buffer, byte paddingByte, int paddingLength)
       {
-          return buffer.Concat(CreatePaddingBuffer(paddingByte, paddingLength));
+          return Concat(buffer, CreatePaddingBuffer(paddingByte, paddingLength));
       }
 
       public static byte[] CreatePaddingBuffer(byte paddingByte, int paddingLength)
       {
-          return Enumerable.Repeat(paddingByte, paddingLength).ToArray();
+    	  byte[] pading = new byte[paddingLength];
+    	  for(int i = 0; i < pading.length; i++) {
+    		  pading[i] = paddingByte;
+    	  }
+    	  
+          return pading;
       }
 
-      public static IEnumerable<byte[]> Chunk(this IEnumerable<byte> source, int chunkSize)
-      {
-          while (source.Any())
-          {
-              yield return source.Take(chunkSize).ToArray();
-              source = source.Skip(chunkSize);
-          }
-      }
+   //   public static Iterable<byte[]> Chunk(Iterable<Byte> source, int chunkSize)
+     // {
+       //   while (source.Aany())
+         // {
+           //   yield return source.Take(chunkSize).ToArray();
+             // source = source.Skip(chunkSize);
+          //}
+     // }
 }
